d03eb804b43a39ec5efc1be2bdce98e68def8e68
KumariSonal19 <sonalkumari5858@gmail.com>
2025-12-09 04:41:10 +0530

    Updated booking-service

--- booking-service/src/main/java/com/flightapp/bookingservice/dto/BookingRequest.java
+++ booking-service/src/main/java/com/flightapp/bookingservice/dto/BookingRequest.java
@@ -1,8 +1,12 @@
 package com.flightapp.bookingservice.dto;
 
+import com.flightapp.bookingservice.enums.MealPreference;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
 import java.util.List;
 
@@ -10,12 +14,31 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class BookingRequest {
+
     private String flightId;
+
+    @NotBlank(message = "User email is required")
+    @Email(message = "Invalid email format")
     private String userEmail;
+
+    @NotBlank(message = "User name is required")
     private String userName;
+
+    @NotNull(message = "Number of seats is required")
+    @Min(value = 1, message = "At least 1 seat must be booked")
     private Integer numberOfSeats;
+
+    @NotEmpty(message = "Passenger list cannot be empty")
+    @Valid 
     private List<PassengerRequest> passengers;
+
+    @NotEmpty(message = "Must select at least one seat")
     private List<String> selectedSeats;
-    private String mealPreference;
+
+    @NotNull(message = "Meal preference is required")
+    private MealPreference mealPreference;
+
+    @NotNull(message = "Journey date is required")
+    @FutureOrPresent(message = "Journey date cannot be in the past")
     private LocalDate journeyDate;
 }
