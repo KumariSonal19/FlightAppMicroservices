4e2f05245fa3b53142e82f727dda3c0813fb9ef3
KumariSonal19 <sonalkumari5858@gmail.com>
2025-12-09 04:52:31 +0530

    Updated flight-service

--- flight-service/src/main/java/com/flightapp/flightservice/dto/FlightSearchRequest.java
+++ flight-service/src/main/java/com/flightapp/flightservice/dto/FlightSearchRequest.java
@@ -1,18 +1,22 @@
 package com.flightapp.flightservice.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import java.time.LocalDate;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class FlightSearchRequest {
-
+    @NotBlank(message="Provide the journey source")
     private String source;
+    @NotBlank(message="Provide the journey destination")
     private String destination;
+    @NotNull(message="Provide the journey departure date")
     private LocalDate departureDate;
+    @NotBlank(message="Provide the journey type")
     private String journeyType;
 }
